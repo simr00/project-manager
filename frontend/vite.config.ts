43f6e05d5c039ff95f959f43fc0d707db4e8e63d
Simranjit Kaur <dcsimran0003@gmail.com>
2026-05-23 16:03:02 +0530

    fix: resolve react-is build issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import path from "path";
 
+
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
 
