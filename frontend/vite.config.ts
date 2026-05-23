e6ae49ab082e969bc4189665b8729ddc56a6b9aa
simr00 <69078605+simr00@users.noreply.github.com>
2026-05-23 15:57:51 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,25 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter()],
+
   resolve: {
     tsconfigPaths: true,
+    alias: {
+      "react-is": path.resolve(__dirname, "node_modules/react-is"),
+    },
+  },
+
+  optimizeDeps: {
+    include: ["react-is"],
+  },
+
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/, /react-is/],
+    },
   },
 });
